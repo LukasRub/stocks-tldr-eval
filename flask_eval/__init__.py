5c421e39204f8300c329c9eca80c89d6638a1d1d
LukasRub <lukas.rubikas@gmail.com>
2021-11-15 19:04:22 +0200

    static issue

--- flask_eval/__init__.py
+++ flask_eval/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True, 
-                static_folder="./static/bower_components", 
+                static_folder="./static", 
                 template_folder="./static")
     app.config.from_mapping(
         SECRET_KEY='dev',
