71111014eedbbe39b55f07c7dc82745707031bac
LukasRub <lukas.rubikas@gmail.com>
2021-11-15 18:09:30 +0200

    Demo

--- flask_eval/schema.sql
+++ flask_eval/schema.sql
@@ -14,3 +14,10 @@ CREATE TABLE stock_evals (
   rating INTEGER NOT NULL,
   optional_reasons TEXT
 );
+
+INSERT INTO artilce_evals (ticker_symbol, rating, optional_reasons) VALUES ("43", "rating-3", "['optional-reason-1', 'optional-reason-2', 'optional-reason-3']");
+INSERT INTO artilce_evals (ticker_symbol, rating, optional_reasons) VALUES ("29", "rating-5", "[]");
+INSERT INTO artilce_evals (ticker_symbol, rating, optional_reasons) VALUES ("oos_4", "rating-5", "[]");
+INSERT INTO artilce_evals (ticker_symbol, rating, optional_reasons) VALUES ("oos_8", "rating-4", "['optional-reason-2']");
+INSERT INTO artilce_evals (ticker_symbol, rating, optional_reasons) VALUES ("11", "rating-4", "['optional-reason-5']");
+INSERT INTO artilce_evals (ticker_symbol, rating, optional_reasons) VALUES ("15", "rating-5", "['optional-reason-1', 'optional-reason-2', 'optional-reason-3', 'optional-reason-4']");
