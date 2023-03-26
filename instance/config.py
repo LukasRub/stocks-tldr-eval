37a6cd84727f1be022341c67fa65cbe7a0914663
LukasRub <lukas.rubikas@gmail.com>
2023-03-26 19:36:49 +0300

    Tidying up the repository

--- instance/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SECRET_KEY = "6921e04b465ea228c15bc09328d48d4314e117909c04ddbf6505043c878d3391"
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_ENGINE_OPTIONS = {
-    'connect_args': {
-        'connect_timeout': 5
-    }
-}
